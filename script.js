0b7be9456164186a8e20e361e6f695f728fca8c0
Andreastephgm <98714194+Andreastephgm@users.noreply.github.com>
2025-04-29 16:44:20 -0500

    update html, css and js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const githubUsername = "Andreastephgm";
 const projectsContainer = document.getElementById("github-projects");
-let currentProjectIndex = 0;
-const projects = [];
 
 const selectedProjects = ['gameStream', 'Pokemon-', 'location-service', 'Contact-Center', 'Animeflix'];
 
@@ -10,23 +8,30 @@ fetch(`https://api.github.com/users/${githubUsername}/repos`)
   .then(repos => {
     repos.forEach(repo => {
       if (!repo.fork && selectedProjects.includes(repo.name)) {
-        projects.push(repo);
-      }
-    });
+        const projectElement = document.createElement("div");
+        projectElement.classList.add("carousel-item");
 
-    showProject(currentProjectIndex);
+        let projectImage = "./images/default.jpg";  
 
-    document.getElementById("nextButton").addEventListener("click", () => {
-      if (currentProjectIndex < projects.length - 1) {
-        currentProjectIndex++;
-        showProject(currentProjectIndex);
-      }
-    });
+        if (repo.name === "gameStream") {
+          projectImage = "./images/game-stream.webp";
+        } else if (repo.name === "Pokemon-") {
+          projectImage = "./images/pikachu.jpg";
+        } else if (repo.name === "location-service") {
+          projectImage = "./images/gps.jpg";
+        } else if (repo.name === "Contact-Center") {
+          projectImage = "./images/diadema.avif";
+        } else if (repo.name === "Animeflix") {
+          projectImage = "./images/animeflix.webp";
+        }
+        projectElement.innerHTML = `
+          <img src="${projectImage}" alt="${repo.name}">
+          <h3>${repo.name}</h3>
+          <p>${repo.description || "Sin descripción disponible."}</p>
+          <a href="${repo.html_url}" target="_blank">Ver en GitHub</a>
+        `;
 
-    document.getElementById("prevButton").addEventListener("click", () => {
-      if (currentProjectIndex > 0) {
-        currentProjectIndex--;
-        showProject(currentProjectIndex);
+        projectsContainer.appendChild(projectElement);
       }
     });
   })
@@ -35,33 +40,6 @@ fetch(`https://api.github.com/users/${githubUsername}/repos`)
     console.error(error);
   });
 
-function showProject(index) {
-  const repo = projects[index];
-  let projectImage = "";
-
-  if (repo.name === "gameStream") {
-    projectImage = "./images/game-stream.webp"; 
-  } else if (repo.name === "Pokemon-") {
-    projectImage = "./images/pikachu.jpg"; 
-  } else if (repo.name === "location-service") {
-    projectImage = "./images/gps.jpg"; 
-  } else if (repo.name === "Contact-Center") {
-    projectImage = "./images/diadema.avif"; 
-  }else if (repo.name === "Animeflix") {
-    projectImage = "./images/animeflix.webp"; 
-  }
-
-  projectsContainer.innerHTML = `
-    <div class="carousel-item">
-      <div class="carousel-content">
-        <h3>${repo.name}</h3>
-        <img src="${projectImage}" alt="${repo.name} image" class="project-image">
-        <p>${repo.description || "Sin descripción disponible."}</p>
-        <a href="${repo.html_url}" target="_blank">Ver en GitHub</a>
-      </div>
-    </div>
-  `;
-}
 
 
 
